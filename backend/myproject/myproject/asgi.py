982d7923215c6f8902e260d46f67f64b81c4776f
Haritha944 <harithams23@gmail.com>
2024-10-23 06:09:42 +0530

    changes in asgi.py

--- backend/myproject/myproject/asgi.py
+++ backend/myproject/myproject/asgi.py
@@ -7,22 +7,42 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+#import os
+#from django.core.asgi import get_asgi_application
+#from channels.auth import AuthMiddlewareStack
+
+#from channels.routing import ProtocolTypeRouter, URLRouter
+#from chat.routing import websocket_urlpatterns
+
+
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+
+#application = ProtocolTypeRouter({
+ #   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#        URLRouter(
+#            websocket_urlpatterns
+#        ),),
+#})
 import os
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.apps import apps
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+django_asgi_app = get_asgi_application()
 from chat.routing import websocket_urlpatterns
 
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        ),),
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
+    ),
 })
